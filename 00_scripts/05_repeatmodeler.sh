f8c6bbfe37bb6efd9a8b16e440b3d14fc42e3b7c
quentin <quentinrougemont@orange.fr>
2023-11-28 14:22:20 +0100

    updated script 05

--- 00_scripts/05_repeatmodeler.sh
+++ 00_scripts/05_repeatmodeler.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+
 #Author: QR
 #Date: 11-2022
 #script to detect repeated sequences
 
-#--- EXTERNAL VARIABLE ---- #
-#input : reference genome and a database name (e.g. the species name)
-#output: several files
+
+#------------- EXTERNAL VARIABLE FROM CONFIG FILE -------------- #
+source ~/config/config
+
+echo TEdatabase is $TEdatabase
+echo "NCBI species is $ncbi_species"
+echo "genome is $genome"
+#echo "database is $haplotype1"
+
 if [ $# -ne 3  ]; then
     echo "USAGE: $0 reference_genome database name rm_unknwon(yes/no)"
     echo -e "Expecting the following parameters:\n
@@ -25,18 +32,17 @@ fi
 
 
 base=$(basename $genome)
-
-#--- USUAL GENERIQ STUFF:  ---#
+#--------------DECLARE THE USUAL GENERIQ STUFF: -----------------#
 TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
 SCRIPT=$0
 NAME=$(basename $0)
-NCPUS=12
 LOG_FOLDER="99_log_files"
 #create log folder
 mkdir $LOG_FOLDER 2>/dev/null
+
 cp $SCRIPT $LOG_FOLDER/"$TIMESTAMP"_"$NAME"
 
-## ----- check compression of fasta  ------ ##
+# ----- check compression of fasta  ------ ##
 #check compression
 if file --mime-type "$genome" | grep -q gzip$; then
    echo "$genome is gzipped"
@@ -47,81 +53,72 @@ else
    genome=$genome
 fi
 
+#this should be deprecated:
+#sed 's/ [0-9A-Za-z=-]*//g' $genome > ${genome%.fa}.simpl.fa
+#genome=${genome%.fa}.simpl.fa
 
-## ----- step 1 -- run repeatmodeler ------ ##
-sed 's/ [0-9A-Za-z=-]*//g' $genome > ${genome%.fa}.simpl.fa
-
-genome=${genome%.fa}.simpl.fa
 base=$(basename $genome )
-
 
 mkdir 05_TE 2>/dev/null
 cd 05_TE
 
-#build db:
+#--------------STEP1 : RUN REPEATMODELER  -----------------------#
+
+##build db:
 BuildDatabase -name $database -engine ncbi ../$genome 2>&1 | tee ../$LOG_FOLDER/buildDatabase.$base.$TIMESTAMP.log
+
 #de novo TE annotations:
-RepeatModeler -pa $NCPUS -engine ncbi -database $database 2>&1 | tee ../$LOG_FOLDER/repeatmodeler_$base.$TIMESTAMP.log
+RepeatModeler -pa 18 -engine ncbi -database $database 2>&1 | tee ../$LOG_FOLDER/repeatmodeler_$base.$TIMESTAMP.log
 
 
+#--------------STEP2 : RUN REPEATMASKER -------------------------#
 
-## ----- step 2 -- run repeatmasker ----- ##
-## ----- step2.1: based on fugrep ----- ##
-FOLDER1=FOLDER1_"${base}"_mask_fugrep.$TIMESTAMP
+# BASED ON DATABASE : 
+FOLDER1=FOLDER1_"${base}"_mask.$TIMESTAMP
 mkdir $FOLDER1
-lib1=/home/quentin/database/fugrep.ref #change according to your repeat lib
-RepeatMasker -pa $NCPUS -e ncbi -lib $lib1 -xsmall -dir "$FOLDER1" ../$genome 2>&1 | tee ../$LOG_FOLDER/repeatmasker_fugrep_$base.$TIMESTAMP.log
-
-## ----- step2.2: based on fngrep ----- ##
-# database 2:
-lib2=/home/quentin/database/fngrep.aa.fasta #edit this if necessary!
-fnbase=$(basename $lib2)
-
-FOLDER2=FOLDER2_"${base}"_mask_fngrep.$TIMESTAMP
-mkdir $FOLDER2
-RepeatMasker -pa $NCPUS -e ncbi -lib $lib2 -xsmall -dir "$FOLDER2" "$FOLDER1"/$base.masked 2>&1 |\
-	tee ../$LOG_FOLDER/repeatmasker_fngrep_$base.$TIMESTAMP.log
+lib1=$TEdatabase 
+RepeatMasker -pa 24 -e ncbi -lib $lib1 -xsmall -dir "$FOLDER1" ../$genome 2>&1 | tee ../$LOG_FOLDER/F1_repeatmasker_$base.$TIMESTAMP.log
 
 
-## ----- step2.3: based on de-novo repeat + repbase ----- ##
-lib3=/home/quentin/database/repbase20.05_aaSeq_cleaned_TE.fa
-lib3base=$(basename $lib3)
-FOLDER3=FOLDER3_"${base}"_mask_"$base"_"$lib3base"."$TIMESTAMP"
-mkdir "$FOLDER3"
+# Based on de-novo repeat + database:
+FOLDER2=FOLDER2_"${base}"_mask.$TIMESTAMP
 
 # test if we keep Unknwon repeat or not
-	## without Unknwon repeat ##
+## without Unknwon repeat ##
 if [[ $rm_unknown = "YES" ]]
 then
         echo "removing Unknown TE Repeats ..."
         awk '$0~/^>/{if(NR>1){print sequence;sequence=""}print $0}$0!~/^>/{sequence=sequence""$0}END{print sequence}' $database-families.fa |\
         sed -e '/Unknown/,+1d' |\
-        cat $lib3 - > $base.no_unknown.repbase.fa
-	lib3cat="$base".no_unknown.repbase.fa
+        cat $lib1 - > $base.no_unknown.repbase.fa
+	libcat="$base".no_unknown.repbase.fa
 else
 	#with known repeat
         echo "keep all candidate TEs... "
-        cat $database-families.fa $lib3 > $base.repbase.fa
-	lib3cat="$base".repbase.fa
+        cat $database-families.fa $lib1 > $base.repbase.fa
+	libcat="$base".repbase.fa
 fi
 
-RepeatMasker -pa $NCPUS -e ncbi -lib $lib3cat -xsmall -dir "$FOLDER3" "$FOLDER2"/"$base".masked.masked 2>&1 |\
-	tee ../$LOG_FOLDER/repeatmasker_$base.repbase20.05.$base.$TIMESTAMP.log
+#run repeatmasker:
+RepeatMasker -pa 18 -e ncbi -lib $libcat -xsmall -dir "$FOLDER2" "$FOLDER1"/"$base".masked 2>&1 |\
+	tee ../$LOG_FOLDER/F2_repeatmasker_$base.$TIMESTAMP.log
 
 
-## ----- step 2.4: based on online data ----- ## 
+## ----- step 2.3: based on online data ----- ## 
 #online database
-FOLDER4=FOLDER4_"${base}"_mask_fungi.$TIMESTAMP
-mkdir "$FOLDER4"
-RepeatMasker -pa $NCPUS -e ncbi -species fungi -xsmall -dir "$FOLDER4"   "$FOLDER3"/"$base".masked.masked.masked 2>&1 | \
-	tee ../$LOG_FOLDER/repeatmasker_fungi.$base.$TIMESTAMP.log
+FOLDER3=FOLDER3_"${base}"_mask.$TIMESTAMP
+mkdir "$FOLDER3"
+
+#run repeatmasker:
+RepeatMasker -pa 18 -e ncbi -species  "${ncbi_species}" -xsmall -dir "$FOLDER3"   "$FOLDER2"/"$base".masked.masked 2>&1 | \
+	tee ../$LOG_FOLDER/F3_repeatmasker_$base.$TIMESTAMP.log
 
 cd ../03_genome
 
 if [[ $rm_unknown = "YES" ]]
 then
-        ln -s ../05_TE/$FOLDER4/$base.masked.masked.masked.masked $database.genome.wholemask_no_unknown.fa
+        ln -s ../05_TE/$FOLDER3/$base.masked.masked.masked genome.wholemask_no_unknown.fa
 else
-        ln -s ../05_TE/$FOLDER4/$base.masked.masked.masked.masked $database.genome.wholemask.fa
+        ln -s ../05_TE/$FOLDER3/$base.masked.masked.masked genome.wholemask.fa
 fi
 
