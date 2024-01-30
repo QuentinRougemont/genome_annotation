8bad8df501d4216e4a72a3f0e486f1bf9cbaaf15
Quentin Rougemont <quentinrougemont@orange.fr>
2024-01-30 18:03:17 +0100

    Update master.sh

--- master.sh
+++ master.sh
@@ -95,7 +95,8 @@ done
 
 echo "------------------------------------------------------------"
 echo "-----check all variables from the configuration file  ------"
-echo "*** ancestral genome is ${anestral_genome} ***"
+echo "*** ancestral genome is ${acnestral_genome} ***"
+echo "*** ancestral gff is ${acnestral_gff} ***"
 echo "*** fasta for genome1 is ${genome1} **** "
 echo "*** fasta for genome2 is ${genome2} **** "
 echo "*** haplotype1 is ${haplotype1} ***"
