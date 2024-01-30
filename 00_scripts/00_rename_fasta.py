e7fbb89bd9859eff0b191c20b7587960901eaa03
Quentin Rougemont <quentinrougemont@orange.fr>
2024-01-30 21:01:55 +0100

    Update 00_rename_fasta.py

--- 00_scripts/00_rename_fasta.py
+++ 00_scripts/00_rename_fasta.py
@@ -5,31 +5,21 @@ import sys
 from Bio import SeqIO
 import re
 input_file=sys.argv[1]
-species_name=sys.argv[2]
+input_file=sys.argv[1]
+current_basename=sys.argv[2]
+new_basename=sys.argv[3]
 
-print(species_name)
-species_name=re.split('[-._]',species_name)
-print(species_name)
-species_name=''.join(species_name)
-print(species_name)
-
-output_file=f'{species_name}.fa'
+output_file=f'{new_basename}.fa'
 
 w1=open(f'{output_file}','w')
 #reads the fasta file
 for contig in SeqIO.parse(f'{input_file}', "fasta"):
-	#for each contig, contig_name = the first element encoded after the ">" (if there is len etc, not included)
-	contig_name=contig.id
-	print(f'old contig name: {contig_name}')
-	#contig_name=re.split('[-._]',contig_name)
-	contig_name=re.split('[-._]',contig_name)[-1]
-	contig_name=''.join(contig_name)
-	# in case there is informations on length etc, removes it to keep just the contig name
-	new_contig_name=species_name+'_'+contig_name
-	print(f'new contig name: {new_contig_name}')
+    contig_name=contig.id
+    print(f'old contig name: {contig_name}')
+    contig_number=re.split(current_basename, contig_name)[1]
+    new_contig_name=new_basename+contig_number
+    print(f'new contig name: {new_contig_name}')
 
-	print('>'+new_contig_name, file=w1)
-	print(contig.seq, file=w1)
+    print('>'+new_contig_name, file=w1)
+    print(contig.seq, file=w1)
 w1.close()
-
-
