b1d17007707c986aa089060a5caf88048c34ccb8
Quentin <quentinrougemont@orange.fr>
2024-02-01 14:13:38 +0100

    fixed typo

--- 00_scripts/07_busco_after_braker.sh
+++ 00_scripts/07_busco_after_braker.sh
@@ -42,6 +42,16 @@ then
    echo "running busco on RNAseq data"
    cd 06_braker/rnaseq
    busco -c8 -o busco_augustus -i $input_fa -l $lineage -m protein #--updata-data #to update the database if there's a warning
-   cd ../../
+   cd ../
+
+   #run for the database:
+   cd 06_braker/ 
+   for i in round* ; do
+	   echo "----- running busco on: $i ------" 
+       cd $i
+       busco -c8 -o busco_augustus -i $input_fa -l $lineage -m protein #--updata-data #to update the database if there's a warning
+       cd ../
+   done 
+
 fi
 
