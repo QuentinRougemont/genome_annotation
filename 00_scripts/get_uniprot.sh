d8c92dda6390465849178edd484b7507471635d1
Quentin Rougemont <quentinrougemont@orange.fr>
2024-02-15 16:44:35 +0100

    Update get_uniprot.sh

--- 00_scripts/get_uniprot.sh
+++ 00_scripts/get_uniprot.sh
@@ -1,6 +1,7 @@
 #downlaod uniprot and index
 
 #this will be used for later quality checks
+rm -r uniprot 2>/dev/null #remove any existing folder to download latest release
 mkdir uniprot
 cd uniprot
 wget https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz
@@ -8,4 +9,3 @@ gunzip uniprot_sprot.fasta.gz
 
 #index:
 diamond makedb --in uniprot_sprot.fasta -d uniprot_sprot.fasta
-
